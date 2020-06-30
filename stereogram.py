ec488f4b9c6cea841f178ed50b3c6e4f17fd8e1b
DanielKulkamp <daniel.kulkamp@gmail.com>
2020-06-30 10:49:21 -0300

    New branch

--- stereogram.py
+++ stereogram.py
@@ -2,28 +2,32 @@ import cv2 as cv
 import sys
 import numpy as np
 import random
-fore = cv.imread("./fore2.png")
 
-if fore is None:
-    sys.exit("Could not read the image.")
 
-depthmap = cv.imread("./cafe.png", cv.IMREAD_GRAYSCALE)
+def main(argv):
+    fore = cv.imread("./fore2.png")
+    if fore is None:
+        sys.exit("Could not read the image.")
 
-final = cv.imread("./fore2.png")
+    depthmap = cv.imread("./cafe.png", cv.IMREAD_GRAYSCALE)
 
-for (i, row) in enumerate(depthmap):
-    for (j, pixel) in enumerate(row):
-        if pixel != 0:
-            #print (f"i={i}, j={j}, pixel={pixel}" )
-            final[i][j] = fore[i][j+30]
+    final = cv.imread("./fore2.png")
+
+    for (i, row) in enumerate(depthmap):
+        for (j, pixel) in enumerate(row):
+            if pixel != 0:
+                #print (f"i={i}, j={j}, pixel={pixel}" )
+                final[i][j] = fore[i][j+30]
             
 
+    cv.imshow("Foreground", fore)
+    cv.imshow("Final", final)
 
-cv.imshow("Foreground", fore)
+    k = cv.waitKey(0)
+    if k == ord("s"):
+        cv.imwrite("./final2.png", final)
+        print(f"saving")
 
-cv.imshow("Final", final)
-
-k = cv.waitKey(0)
-if k == ord("s"):
-    cv.imwrite("./final2.png", final)
-    print(f"saving")
+if __name__ == '__main__':
+    print(sys.argv)
+    main(sys.argv[1:])
