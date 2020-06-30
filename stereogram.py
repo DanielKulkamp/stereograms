ffbf9e90d04b000a2dcb33d8a3b092671e47fc5c
DanielKulkamp <daniel.kulkamp@gmail.com>
2020-06-30 11:31:35 -0300

    Input files working in the args

--- stereogram.py
+++ stereogram.py
@@ -5,21 +5,38 @@ import random
 
 
 def main(argv):
-    fore = cv.imread("./fore2.png")
+    if len(argv) < 3:
+        print("Usage: stereogram <inputfile> <depthmap> <outputfile> [-f=<forceshiftvalue>] [-r=<WxH>]")
+
+    fore = cv.imread(argv[0])
     if fore is None:
         sys.exit("Could not read the image.")
 
-    depthmap = cv.imread("./cafe.png", cv.IMREAD_GRAYSCALE)
+    depthmap = cv.imread(argv[1], cv.IMREAD_GRAYSCALE)
+    if depthmap is None:
+        sys.exit("Could not read the depthmap image.")
+    
+    final = cv.imread(argv[0])
 
-    final = cv.imread("./fore2.png")
+    for arg in argv[3:]:
+        if arg[:3] == '-f=':
+            try:
+                force = int(arg[3:])
+            except ValueError as e:
+                print(f"{e}")
+                sys.exit("invalid force shift input")
+            except:
+                sys.exit("An exception occurred")
 
     for (i, row) in enumerate(depthmap):
         for (j, pixel) in enumerate(row):
             if pixel != 0:
-                #print (f"i={i}, j={j}, pixel={pixel}" )
-                final[i][j] = fore[i][j+30]
-            
+                if force is None:
+                    final[i][j] = fore[i][j+depthmap[i][j]]
+                else:
+                    final[i][j]~= fore[i][j+force]
 
+            
     cv.imshow("Foreground", fore)
     cv.imshow("Final", final)
 
