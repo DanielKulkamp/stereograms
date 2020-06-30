e8c97bf450c76bca255f55a84b9aed84fe0a96b2
DanielKulkamp <daniel.kulkamp@gmail.com>
2020-06-30 10:41:20 -0300

    almost ok

--- stereogram.py
+++ stereogram.py
@@ -2,28 +2,28 @@ import cv2 as cv
 import sys
 import numpy as np
 import random
-fore = cv.imread("./fore2133x1200.png")
+fore = cv.imread("./fore2.png")
 
 if fore is None:
     sys.exit("Could not read the image.")
 
-depthmap = cv.imread("./depthmap2133x1200.png", cv.IMREAD_GRAYSCALE)
+depthmap = cv.imread("./cafe.png", cv.IMREAD_GRAYSCALE)
 
-final = cv.imread("./fore2133x1200.png")
+final = cv.imread("./fore2.png")
 
 for (i, row) in enumerate(depthmap):
     for (j, pixel) in enumerate(row):
-        final[i][j] = fore[i][j+pixel]
         if pixel != 0:
             #print (f"i={i}, j={j}, pixel={pixel}" )
-            final[i][j] = fore[i][(j+pixel)%100]
+            final[i][j] = fore[i][j+30]
             
 
 
 cv.imshow("Foreground", fore)
-cv.imshow("Noise", noise)
+
 cv.imshow("Final", final)
 
 k = cv.waitKey(0)
 if k == ord("s"):
-    cv.imwrite("./final.png", final)
+    cv.imwrite("./final2.png", final)
+    print(f"saving")
